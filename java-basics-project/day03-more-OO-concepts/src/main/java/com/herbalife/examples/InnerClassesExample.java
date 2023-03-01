42c4c658e2adf1a11bfaebce20cd3ec9e693e054
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-01 10:52:03 +0530

    Day03

---
+++ java-basics-project/day03-more-OO-concepts/src/main/java/com/herbalife/examples/InnerClassesExample.java
@@ -0,0 +1,47 @@
+package com.herbalife.examples;
+
+public class InnerClassesExample {
+    public static void main(String[] args) {
+        TV sony = new TV();
+        TV.Remote sonyRemote = sony.new Remote(); //You can create an inner class object only using the outer class object
+        sonyRemote.increaseVolume();
+        sonyRemote.decreaseVolume();
+    }
+}
+
+class TV {
+    private int volume;
+
+    public void increaseVolume() {
+        volume++;
+    }
+
+    public void decreaseVolume() {
+        volume--;
+    }
+
+    //Inner class can access private members of the parent/outer class
+    class Remote {
+        public void increaseVolume() {
+            volume++;
+        }
+
+        public void decreaseVolume() {
+            volume--;
+        }
+
+        class Chip {
+            class MicroPart {
+            }
+        }
+    }
+
+    class Button {
+    }
+
+    class CustomRemote extends Remote {
+
+    }
+    static class Wire {}
+}
+class MyWire extends TV.Wire {}
