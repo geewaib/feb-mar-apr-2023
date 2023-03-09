63b1c4c785a33ff8c17f4fdfed056285e8bf984b
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-09 10:34:16 +0530

    Day 08

---
+++ java-basics-project/day08-misc/src/main/java/com/herbalife/examples/JdbcBasics.java
@@ -0,0 +1,83 @@
+package com.herbalife.examples;
+
+import java.sql.*;
+
+public class JdbcBasics {
+    public static void main(String[] args) throws SQLException {
+        //insertPerson("Ram", "Narain", 32);
+        insertPersonUsingPreparedStatement("Joe", "Martin", 35);
+        printAllPersons();
+    }
+
+    private static void printAllPersons() throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Type.GetType("");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            Statement statement = connection.createStatement();
+            String query = "select * from persons";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                int age = rs.getInt("age");
+                System.out.println(id + ", " + firstName + " " + lastName + ", " + age);
+            }
+            rs.close();
+            statement.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static void insertPersonUsingPreparedStatement(String firstName, String lastName, int age) throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Type.GetType("");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            String query = "insert into persons(first_name, last_name, age) values(?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.setInt(3, age);
+            statement.executeUpdate(query);
+            statement.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+    //Load the driver
+    //Create connection
+    //Create a statement
+    //Execute query
+    //Close the statement
+    //Close the connection
+    private static void insertPerson(String firstName, String lastName, int age) throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Type.GetType("");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            Statement statement = connection.createStatement();
+            String query = "insert into persons(first_name, last_name, age) values('" + firstName + "','" + lastName + "'," + age + ")";
+            statement.executeUpdate(query);
+            statement.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+}
