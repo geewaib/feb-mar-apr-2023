b8f6d9b2195a537e7a4241d87015ccad2740f9c3
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-15 11:48:05 +0530

    Day10

--- spring-project/jpa-basics-day11/src/main/java/com/herbalife/jpabasicsday11/JpaBasicsDay11Application.java
+++ spring-project/jpa-basics-day11/src/main/java/com/herbalife/jpabasicsday11/JpaBasicsDay11Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -19,30 +20,42 @@ public class JpaBasicsDay11Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("********************************************");
+        System.out.println(personsDao.getClass().getName());
+        System.out.println("====interfaces====");
+        for (Class cls : personsDao.getClass().getInterfaces()) {
+            System.out.println(cls.getName());
+        }
+        System.out.println("====methods====");
+        for (Method method : personsDao.getClass().getMethods()) {
+            System.out.println(method.getName());
+        }
+        System.out.println("********************************************");
+
 //        Person person = new Person();
 //        person.setFirstName("Steffi");
 //        person.setLastName("Graf");
 //        person.setAge(54);
 //        personsDao.save(person);
 
-        personsDao
-                .findAll()
-                .forEach(System.out::println);
+//        personsDao
+//                .findAll()
+//                .forEach(System.out::println);
 
 
         //update a person's age
-        Optional<Person> optionalPerson =  personsDao.findById(1);
-        if(optionalPerson.isPresent()) {
-            Person p = optionalPerson.get();
-            p.setAge(p.getAge() + 1);
-            personsDao.save(p);
-        }
+//        Optional<Person> optionalPerson =  personsDao.findById(1);
+//        if(optionalPerson.isPresent()) {
+//            Person p = optionalPerson.get();
+//            p.setAge(p.getAge() + 1);
+//            personsDao.save(p);
+//        }
 
         //delete a person
-        Optional<Person> optPerson =  personsDao.findById(5);
-        if(optPerson.isPresent()) {
-            Person p = optPerson.get();
-            personsDao.delete(p);
-        }
+//        Optional<Person> optPerson =  personsDao.findById(5);
+//        if(optPerson.isPresent()) {
+//            Person p = optPerson.get();
+//            personsDao.delete(p);
+//        }
     }
 }
