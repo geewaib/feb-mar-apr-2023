69783daa500e66c5ddc4247d2f6c7f09cac102e0
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-23 12:04:01 +0530

    Day15

--- herbalife-web-app/src/main/java/com/herbalife/AdditionServlet.java
+++ herbalife-web-app/src/main/java/com/herbalife/AdditionServlet.java
@@ -1,20 +1,23 @@
 package com.herbalife;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/square")
-public class SquareServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/sum")
+public class AdditionServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int number = Integer.parseInt(request.getParameter("number"));
-        int result = number * number;
+        int number1 = Integer.parseInt(request.getParameter("number1"));
+        int number2 = Integer.parseInt(request.getParameter("number2"));
+        int result = number1 + number2;
         PrintWriter printWriter = response.getWriter();
-        printWriter.println("<h1>Square of " + number + " is " + result + "</h1>");
+        printWriter.println("<h1>Sum of " + number1 + " and " + number2 + " is " + result + "</h1>");
     }
 
 }
