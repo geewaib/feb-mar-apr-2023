76ff1e790f4fcc81765ac1ee43610267afa1f358
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-27 10:23:01 +0530

    Day16

---
+++ spring-boot-kafka/sample-producer/src/main/java/com/herbalife/sampleproducer/SampleProducerApplication.java
@@ -0,0 +1,13 @@
+package com.herbalife.sampleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProducerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka/sample-producer/src/test/java/com/herbalife/sampleproducer/SampleProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbalife.sampleproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
