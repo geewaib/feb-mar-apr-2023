ffda3fa3f3fef463ed4c8ab0996774e996ba2c8e
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-28 10:46:40 +0530

    Day17

--- spring-boot-kafka/more-on-consumer/src/main/java/com/herbalife/moreonconsumer/SampleConsumer.java
+++ spring-boot-kafka/more-on-consumer/src/main/java/com/herbalife/moreonconsumer/SampleConsumer.java
@@ -1,8 +1,37 @@
 package com.herbalife.moreonconsumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleConsumer {
-    
+
+    @KafkaListener(topics = "tuesday-topic", groupId = "tuesday-topic-consumer-group-1")
+    public void process1(ConsumerRecord record) {
+        String data = record.value().toString();
+        System.out.println("Topic: " + record.topic());
+        System.out.println("Offset: " + record.offset());
+        record.headers().forEach(header -> {
+            System.out.println(header.key() + ": " + new String(header.value()));
+        });
+        System.out.println("********Consumer 1: " + data + " from partition: " + record.partition());
+    }
+
+    @KafkaListener(topics = "tuesday-topic", groupId = "tuesday-topic-consumer-group-1")
+    public void process2(String data) {
+        System.out.println("********Consumer 2: " + data);
+    }
+
+    @KafkaListener(topics = "tuesday-topic", groupId = "tuesday-topic-consumer-group-1")
+    public void process3(String data) {
+        System.out.println("********Consumer 3: " + data);
+    }
+
+    @KafkaListener(topics = "tuesday-topic", groupId = "tuesday-topic-consumer-group-1")
+    public void process4(String data) {
+        System.out.println("********Consumer 4: " + data);
+    }
+
 }
--- spring-boot-kafka/more-on-producer/src/main/java/com/herbalife/moreonproducer/SampleController.java
+++ spring-boot-kafka/more-on-producer/src/main/java/com/herbalife/moreonproducer/SampleController.java
@@ -1,5 +1,7 @@
 package com.herbalife.moreonproducer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
     @PostMapping("/{data}")
     public void postData(@PathVariable String data) {
-
+        kafkaTemplate.send("tuesday-topic", data);
     }
 }
