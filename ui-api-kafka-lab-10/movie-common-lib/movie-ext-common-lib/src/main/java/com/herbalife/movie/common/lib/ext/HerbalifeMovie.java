8a2fa2638453118e11030f29ae04edc627f571ef
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-30 10:57:56 +0530

    Day19

---
+++ ui-api-kafka-lab-10/movie-common-lib/movie-ext-common-lib/src/main/java/com/herbalife/movie/common/lib/ext/HerbalifeMovie.java
@@ -0,0 +1,8 @@
+package com.herbalife.movie.common.lib.ext;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class HerbalifeMovie {
+
+}
