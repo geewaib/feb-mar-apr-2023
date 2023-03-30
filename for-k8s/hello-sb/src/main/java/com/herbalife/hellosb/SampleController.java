8efa3453208728281cbe4778888c8e94a115d0e4
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-03-30 12:11:49 +0530

    Day19

--- for-k8s/hello-sb/src/main/java/com/herbalife/hellosb/SampleController.java
+++ for-k8s/hello-sb/src/main/java/com/herbalife/hellosb/SampleController.java
@@ -1,11 +1,21 @@
 package com.herbalife.hellosb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+
+    @Value("${welcome}")
+    private String welcome;
+
+    @GetMapping("/welcome")
+    public String getWelcome() {
+        return welcome;
+    }
+
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         return "Hello " + name;
